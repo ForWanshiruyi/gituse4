0491a25678b60049baf1727742048977d2daa3cf
wanshiruyi <wanshiruyi.com>
2022-07-17 01:07:52 +0800

    111-co

---
+++ src/com/mystudy/Student.java
@@ -0,0 +1,11 @@
+package com.mystudy;
+
+/**
+ * @author :dingyb
+ * @date : 2022/7/17 1:06
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
